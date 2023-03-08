ae1115dfce31d1eb74a5a3062971f9abcff56216
Yanis <yayaezzannati@gmail.com>
2023-03-08 09:23:28 +0100

    class animal

---
+++ AnimalCSharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Xml.Linq;
+
+
+class Animal
+{
+    public string Name = null;
+
+    static void Main(string[] args)
+    {
+        Move();
+        Sleep();
+        Console.ReadLine();
+    }
+
+    public void animalconstructeur(string Name)
+    {
+        this.Name = Name;
+    }
+
+    public static void Move()
+    {
+        Console.WriteLine("Move");
+	}
+
+    public static void Sleep()
+    {
+        Console.WriteLine("Sleep");
+    }
+}
