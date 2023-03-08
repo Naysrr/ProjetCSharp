4ebe2fb81645d3bce39b334c2d97a958533397fa
Yanis <yayaezzannati@gmail.com>
2023-03-08 11:47:01 +0100

    animal

--- AnimalCSharp/ConsoleApp1/ConsoleApp1/Program.cs
+++ AnimalCSharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,31 +1,65 @@
 ﻿using System;
 using System.Net;
 using System.Xml.Linq;
+using System;
 
-
-class Animal
+public abstract class Animal //ne peut pas être instancié directement
 {
-    public string Name = null;
+    public string Name { get; set; }
 
-    static void Main(string[] args)
+    public Animal()
     {
-        Move();
-        Sleep();
-        Console.ReadLine();
+        this.Name = "triceratops";
     }
 
-    public void animalconstructeur(string Name)
+    public Animal(string name)
     {
-        this.Name = Name;
+        this.Name = name; // permet de spécifier le nom de l'animal lors de sa créa
     }
 
-    public static void Move()
-    {
-        Console.WriteLine("Move");
-	}
+    public abstract void Move(); // doit être implémenter par les classes dérivées
 
-    public static void Sleep()
+    public new void Sleep()
     {
-        Console.WriteLine("Sleep");
+        Console.WriteLine(Name + " rompiche");
+    }
+}
+
+public class Griffin : Animal // classe dérivée de Animal
+{
+    public Griffin() : base()
+    {
+        this.Name = "Griffin";
+    }
+
+    public Griffin(string name) : base(name)
+    {
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine(Name + "en elytra");
+    }
+}
+
+public class Dragon : Animal // classe dérivée de Animal
+{
+    public Dragon() : base()
+    {
+        this.Name = "Dragon";
+    }
+
+    public Dragon(string name) : base(name)
+    {
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine(Name + " craches du feu en volant");
+    }
+
+    public new void Sleep()
+    {
+        Console.WriteLine(Name + "rrrrrr piche");
     }
 }
